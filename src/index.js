acfb4103eb9e69fdf21e423218a0776dd03c9462
sarahsweat <sarahsweat@gmail.com>
2017-07-17 17:16:52 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import ImageSlider from './components/ImageSlider';
-import Bomb from './components/Bomb';
+import ImageSlider from './components/ImageSlider'
+import Bomb from './components/Bomb'
 
 ReactDOM.render(
   <div>
@@ -10,4 +10,4 @@ ReactDOM.render(
     <Bomb initialCount={120} />
   </div>,
   document.getElementById('root')
-);
+)
